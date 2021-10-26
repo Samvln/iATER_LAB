3bf6828cf361172e3fd7a2f97c57dd58dfc7b56b
Sam <93126466+Samvln@users.noreply.github.com>
2021-10-26 11:12:20 +0700

    Add files via upload

--- Diamond.java
+++ Diamond.java
@@ -1,10 +1,10 @@
 package iATER;
 
+import java.util.Scanner;
+
 public class Diamond {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int num = 6;
+	static void my_method(int num) {
 
 		for (int i = 1; i <= num; i++) {
 			for (int j = num; j > i; j--) {
@@ -15,7 +15,7 @@ public class Diamond {
 			}
 			System.out.println("");
 		}
-		for (int i = num; i > 0; i--) {
+		for (int i = num - 1; i > 0; i--) {
 			for (int j = i; j < num; j++) {
 				System.out.print(" ");
 			}
@@ -24,6 +24,18 @@ public class Diamond {
 			}
 			System.out.println("");
 		}
+	}
+
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the number:");
+		int number = scanner.nextInt();
+
+		my_method(number);
+
 	}
 
 }
